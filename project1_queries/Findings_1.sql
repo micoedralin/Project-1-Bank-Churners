6d57652e2e898b59439ae4c93806fb1c0497ba04
micoedralin <micocarledralin@gmail.com>
2026-02-03 12:48:49 -0500

    commit

---
+++ project1_queries/Findings_1.sql
@@ -0,0 +1,11 @@
+-- Q1: What is the difference between existing customers amd attrited customers when it comes to their credit limit? --
+-- A1: Existing customers, on average, have higher credit limit than attrited customers --
+SELECT
+    attrition_flag,
+    AVG(credit_limit) AS avg_credit_limit
+FROM
+    bank_churners
+GROUP BY
+    attrition_flag
+ORDER BY
+    avg_credit_limit DESC;
